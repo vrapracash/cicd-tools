64b7fd074e2827f155b3868820b0c7c82d838ffe
vrapracash <veeraprakash.bg@gmail.com>
2025-08-22 10:15:57 +0530

    script-update

--- provider.tf
+++ provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "81s-remote-state"
+    bucket = "vrapracash-1"
     key    = "expense-tools"
     region = "us-east-1"
     dynamodb_table = "81s-locking"
