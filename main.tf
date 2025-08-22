da205d60c7e9d8d881b95f14ab1b6d5dc09906e2
vrapracash <veeraprakash.bg@gmail.com>
2025-08-22 11:01:29 +0530

    sg-vpc-changed

--- main.tf
+++ main.tf
@@ -28,8 +28,8 @@ module "jenkins_agent" {
   name = "jenkins-agent"
 
   instance_type          = "t3.small"
-  vpc_security_group_ids = ["sg-0fea5e49e962e81c9"]
-  subnet_id = "subnet-0ea509ad4cba242d7"
+  vpc_security_group_ids = ["sg-0174653de207fa302"]
+  subnet_id = "subnet-0e50ad42633a64074"
   ami = data.aws_ami.ami_info.id
   user_data = file("jenkins-agent.sh")
   tags = {
