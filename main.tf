d54d4dc1d0e16de25d49790e8e97e46851f654d7
vrapracash <veeraprakash.bg@gmail.com>
2025-09-01 23:23:56 +0530

    changed gp3-gp2

--- main.tf
+++ main.tf
@@ -15,11 +15,13 @@ module "jenkins" {
   # Define the root volume size and type
   root_block_device = {
       volume_size = 50       # Size of the root volume in GB
-      volume_type = "gp3"    # General Purpose SSD (you can change it if needed)
+      volume_type = "gp2"    # General Purpose SSD (you can change it if needed)
       delete_on_termination = true  # Automatically delete the volume when the instance is terminated
     }
 
 }
+
+
 
 module "jenkins_agent" {
   source  = "terraform-aws-modules/ec2-instance/aws"
@@ -37,7 +39,7 @@ module "jenkins_agent" {
 
   root_block_device = {
       volume_size = 50       # Size of the root volume in GB
-      volume_type = "gp3"    # General Purpose SSD (you can change it if needed)
+      volume_type = "gp2"    # General Purpose SSD (you can change it if needed)
       delete_on_termination = true  # Automatically delete the volume when the instance is terminated
     }
   
